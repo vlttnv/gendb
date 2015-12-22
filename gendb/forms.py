fb6ca69204c26c828690142c52e95f211390d361
Valentin Tunev <hdbuster@gmail.com>
2015-12-22 13:48:05 +0100

    Simple login/logout

--- gendb/forms.py
+++ gendb/forms.py
@@ -9,3 +9,8 @@ class RegisterForm(Form):
     email = StringField('email', id='email', validators=[DataRequired()])
     pw1 = PasswordField('pw1', id='pw1', validators=[DataRequired()])
     pw2 = PasswordField('pw2', id='pw2', validators=[DataRequired()])
+
+
+class LoginForm(Form):
+    email = StringField('email', id='email', validators=[DataRequired()])
+    pw1 = PasswordField('pw1', id='pw1', validators=[DataRequired()])
--- gendb/views/home.py
+++ gendb/views/home.py
@@ -2,16 +2,18 @@
 
 from hashlib import sha1
 
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, flash
 from flask.ext.login import login_required, login_user, logout_user
+from sqlalchemy.orm.exc import NoResultFound
 
 from gendb.extensions import db
-from gendb.forms import RegisterForm
+from gendb.forms import RegisterForm, LoginForm
 from gendb.models import User
 
 home_bp = Blueprint('home_bp', __name__)
 
 
+@login_required
 @home_bp.route('/')
 @login_required
 def index():
@@ -36,3 +38,34 @@ def register():
 
         return redirect(url_for('home_bp.index'))
     return render_template('/home/register.html', form=form)
+
+
+@home_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    """Remder login form and login user."""
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        try:
+            u = User.query.filter_by(email=form.email.data).one()
+            password = sha1(form.pw1.data.encode('utf-8')).hexdigest()
+            if u.password == password:
+                login_user(u)
+                return redirect(url_for('home_bp.index'))
+            else:
+                flash('Incorrect username or password', 'danger')
+                return redirect(url_for('home_bp.login'))
+
+        except NoResultFound:
+            flash('User not found.', 'danger')
+            return redirect(url_for('home_bp.login'))
+
+    return render_template('/home/login.html', form=form)
+
+
+@home_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home_bp.login'))
