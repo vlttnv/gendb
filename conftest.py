8a0e42104abccba6746f7a151047d34d51f7c6f8
Valentin Tunev <hdbuster@gmail.com>
2015-12-25 13:06:17 +0100

    Modified test structure

--- conftest.py
+++ conftest.py
@@ -1,13 +1,65 @@
+import os
 import pytest
+
 from gendb.app import create_app
+from gendb.extensions import db as _db
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+TESTDB = os.path.join(basedir, 'gendb_app.db')
+TEST_DATABASE_URI = 'sqlite:///' + TESTDB
 
 
-@pytest.fixture
-def app():
+@pytest.fixture(scope='session')
+def app(request):
+    """Session-wide test `Flask` application."""
     app = create_app()
+    app.config['TESTING'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = TEST_DATABASE_URI
+    app.config['WTF_CSRF_ENABLED'] = False
+
+    # Establish an application context before running the tests.
+    ctx = app.app_context()
+    ctx.push()
+
+    def teardown():
+        ctx.pop()
+
+    request.addfinalizer(teardown)
     return app
 
 
-@pytest.fixture
-def test_app(client):
-    assert client.get(url_for('home_bp.index')).status_code == 200
+@pytest.fixture(scope='session')
+def db(app, request):
+    """Session-wide test database."""
+    if os.path.exists(TESTDB):
+        os.unlink(TESTDB)
+
+    def teardown():
+        _db.drop_all()
+        os.unlink(TESTDB)
+
+    _db.app = app
+    _db.create_all()
+
+    request.addfinalizer(teardown)
+    return _db
+
+
+@pytest.fixture(scope='function')
+def session(db, request):
+    """Creates a new database session for a test."""
+    connection = db.engine.connect()
+    transaction = connection.begin()
+
+    options = dict(bind=connection)
+    session = db.create_scoped_session(options=options)
+
+    db.session = session
+
+    def teardown():
+        transaction.rollback()
+        connection.close()
+        session.remove()
+
+    request.addfinalizer(teardown)
+    return session
